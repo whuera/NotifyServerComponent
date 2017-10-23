ee9a3651676103bbb248146e9b88231dff627314
William Huera <whuera@gmail.com>
2017-10-23 14:50:12 -0500

    update component

--- mailsender/src/main/java/com/mp/mail/ControllerEmailTemplate.java
+++ mailsender/src/main/java/com/mp/mail/ControllerEmailTemplate.java
@@ -7,6 +7,8 @@ import org.apache.velocity.exception.MethodInvocationException;
 import org.apache.velocity.exception.ParseErrorException;
 import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.AbstractRefreshableConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.app.modelo.ContactMail;
@@ -14,91 +16,64 @@ import com.app.modelo.InfoEnterprice;
 import com.app.service.impl.ServiceContactSendMail;
 import com.app.service.impl.ServiceInfoEnterprice;
 
-
 /**
  * The Class SpringEmailTemplateExample.
  */
-public class SpringEmailTemplateExample {
-	
+public class ControllerEmailTemplate {
+
 	/**
 	 * Mail sender helper.
 	 *
-	 * @param mailReceptor the mail receptor
-	 * @param company the company
+	 * @param mailReceptor
+	 *            the mail receptor
+	 * @param company
+	 *            the company
 	 */
 
-	
-	public static void mailSenderHelper(String company)
-	{
+	public static void mailSenderHelper(String company) {
 		String flgTemplate = "emailtemplate.vm";
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring-beans.xml");
 		System.setProperty("host", "smtpout.secureserver.net");
 		System.setProperty("port", "465");
 		System.setProperty("username", "it-services@mobilpymes.cloud");
-		System.setProperty("password","*weho7755*");
-		
+		System.setProperty("password", "*weho7755*");
+		((AbstractRefreshableConfigApplicationContext) context).setConfigLocation("spring-beans.xml");
+		((AbstractApplicationContext) context).refresh();
+
 		Mailer mailer = (Mailer) context.getBean("mailer");
 
 		ServiceContactSendMail serviceContactSendMail = new ServiceContactSendMail();
 		ServiceInfoEnterprice serviceInfoEmp = new ServiceInfoEnterprice();
-		InfoEnterprice infoEnterprice =  serviceInfoEmp.getInfoEnterprice(company);
-		//Contacto contacto = new Contacto();
+		InfoEnterprice infoEnterprice = serviceInfoEmp.getInfoEnterprice(company);
 		ArrayList<ContactMail> listContacto = new ArrayList<ContactMail>();
-		listContacto = serviceContactSendMail.getContacts(infoEnterprice.getOccurrences());
-/*
-		contacto.setPrimerNombre("William");
-		contacto.setPrimerApellido("Huera");
-		contacto.setMailContacto("whuera@gmail.com");
 
-		listContacto.add(contacto);
-
-		Contacto contacto2 = new Contacto();
-		contacto2.setPrimerNombre("Usuario");
-		contacto2.setPrimerApellido("del Sistema Mobilpymes");
-		
-		if(mailReceptor!=null){
-		contacto2.setMailContacto(mailReceptor);
-		}else{
-			contacto2.setMailContacto("eduardohuera@gmail.com");
+		boolean valCompany = true;
+		if (!company.equalsIgnoreCase("DUMMY")) {
+			listContacto = serviceContactSendMail.getContacts(infoEnterprice.getOccurrences());
+		} else if (company.equalsIgnoreCase("DUMMY")) {
+			ContactMail dummyMail = new ContactMail();
+			dummyMail.setId(10213);
+			dummyMail.setEmailcontact("whuera@gmail.com");
+			dummyMail.setNames("William Huera");
+			listContacto.add(dummyMail);
+			valCompany = false;
 		}
-			
-		//listContacto.add(contacto2);
-		
-		*/
 
 		Mail mail = new Mail();
-		/*
-		mail.setMailFrom("publicidadmobilpymes@gmail.com");
-		mail.setMailBcc("whuera@gmail.com");
-		mail.setMailTo("freddy.lojan@etafashion.com");
-		mail.setMailSubject("Promociones Mobilpymes.com");
-		mail.setTemplateName("emailtemplate.vm");
-		*/
-		//flgTemplate = company.equalsIgnoreCase("aireec")?"emailtemplateaire.vm":"emailtemplate.vm";
-		/*
-		if (company.equalsIgnoreCase("aireec"))
-		{
-			flgTemplate = "emailtemplateaire.vm";
-		}else if(company.equalsIgnoreCase("smartbid"))
-		{
-			flgTemplate = "emailtemplatesmartbid.vm";
-		}else if (company.equalsIgnoreCase("mobilpymescloud")){
-			flgTemplate = "emailtemplateservicemanager.vm";
-		}else if (company.equalsIgnoreCase("mobilpymes")){
-			flgTemplate = "emailtemplate.vm";
-		}
-		*/
+
 		flgTemplate = infoEnterprice.getNameTemplate();
-		
+
 		for (ContactMail contactoFinal : listContacto) {
 			mail.setMailFrom(infoEnterprice.getEmpemail());
 			mail.setMailTo(contactoFinal.getEmailcontact());
-			//mail.setMailTo("whuera@gmail.com");
-			mail.setMailSubject("Sistema Data+ (Micro Buro de Información y relación comercial) "+company);
+
+			mail.setMailSubject(infoEnterprice.getEmailSubject());
 			mail.setTemplateName(flgTemplate);
 			try {
 				mailer.sendMail(mail, contactoFinal.getNames(), "");
-				serviceContactSendMail.updateStatusMail(contactoFinal.getId());
+				if (valCompany) {
+					serviceContactSendMail.updateStatusMail(contactoFinal.getId());
+				}
 			} catch (ResourceNotFoundException e) {
 
 				e.printStackTrace();
@@ -115,16 +90,6 @@ public class SpringEmailTemplateExample {
 			contactoFinal = null;
 		}
 
-		
 	}
-	/*
 
-	public static void main(String[] args) {
-		// conn = new conexionOracle();
-		if (args[0].equalsIgnoreCase("-receptor") && !args[1].equalsIgnoreCase(null)){
-			String mailRecord = !args[1].equalsIgnoreCase(null)?args[1].trim():"eduardohuera@gmail.com";
-		SpringEmailTemplateExample.mailSenderHelper(mailRecord);
-		}
-	}
-*/
 }
